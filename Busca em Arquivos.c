edc51b3e8b0af46b85714153fc9a1e8e34087a31
GalileuGalilei <alfredocossetin@outlook.com>
2022-06-26 20:53:45 -0300

    finalizado(provavelmente)

--- Busca em Arquivos.c
+++ Busca em Arquivos.c
@@ -1,3 +1,4 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -38,57 +39,177 @@ typedef struct {
     int nota2;
     int excluido;
 } reg_aluno;
+
 //TODO: Implementar um indice (em memoria) para facilitar a busca dos registros do arquivo
 //TODO: Alterar as funcoes de busca, insercao e exclusao para usar o indice
+
+//estrutura de pilha
 typedef struct {
     int matr;
     int pos_seek;
+    struct indice* prox; 
 } indice;
+
+void indice_insere(indice** raiz,int matr, int pos_seek)
+{
+    indice* new_ind = (indice*)malloc(sizeof(indice));
+    new_ind->matr = matr;
+    new_ind->pos_seek = pos_seek;
+
+    new_ind->prox = *raiz;
+    *raiz = new_ind;
+}
+
+void indice_exclui(indice** raiz, int matr)
+{
+    if (*raiz == NULL)
+    {
+        return;
+    }
+
+    indice* aux;
+    if ((*raiz)->matr == matr)
+    {
+        aux = (*raiz)->prox;
+        free(*raiz);
+        *raiz = aux;
+        return;
+    }
+
+    indice* ant = *raiz;
+    for (indice* i = *raiz; i != NULL; i = i->prox)
+    {
+        if (i->matr == matr)
+        {
+            aux = i->prox;
+            free(i);
+            ant->prox = aux;
+            return;
+        }
+        ant = i;
+    }
+}
+
+//retorna o pos_seek
+int indice_pesquisa(indice* raiz, int matr)
+{
+    for (indice* i = raiz; i != NULL; i = i->prox)
+    {
+        if (i->matr == matr)
+        {
+            return i->pos_seek;
+        }
+    }
+
+    return -1;
+}
+
+void indice_inicializa(indice** ativos, indice** excluidos, FILE* arq)
+{
+    reg_aluno aluno;
+    rewind(arq);
+
+    while (fread(&aluno, sizeof(aluno), 1, arq))
+    {
+        if (aluno.excluido == 0)
+        {
+            indice_insere(ativos, aluno.matr, ftell(arq) - sizeof(aluno));
+        }
+        else
+        {
+            indice_insere(excluidos, aluno.matr, ftell(arq) - sizeof(aluno));
+        }
+    }
+}
 
 void mostra(FILE* arq) {
     reg_aluno aluno;
     rewind(arq);
+
     while (fread(&aluno, sizeof(aluno), 1, arq))
+    {
         if (aluno.excluido == 0)
-            printf("%d\t%s\t%d\t%d\n", aluno.matr, aluno.nome, aluno.nota1, aluno.nota2);
-}
-int pesquisa(FILE* arq, int matr, reg_aluno* al) {
-    reg_aluno aluno;
-    rewind(arq);
-    while (fread(&aluno, sizeof(aluno), 1, arq))
-        if (aluno.matr == matr && aluno.excluido == 0) {
-            *al = aluno;
-            return 1;
+        {
+            printf("%d\t %s\t %d\t %d\n", aluno.matr, aluno.nome, aluno.nota1, aluno.nota2);
         }
-    return 0;
-}
-void exclui(FILE* arq, int matr) {
-    reg_aluno aluno;
-    if (pesquisa(arq, matr, &aluno)) {
-        int excl = 1;
-        printf("Excluindo: %s\n", aluno.nome);
-        fseek(arq, -1 * sizeof(int), SEEK_CUR);
-        fwrite(&excl, sizeof(int), 1, arq);
-        fflush(arq);
     }
 }
-void inclui(FILE* arq) {
+int pesquisa(FILE* arq, int matr, reg_aluno* al, indice* ativos) {
     reg_aluno aluno;
-    fseek(arq, 0, SEEK_END);
-    //printf("Pos=%d", ftell(arq));
+    int pos_seek = indice_pesquisa(ativos, matr);
+
+    if (pos_seek == -1)
+    {
+        return 0;
+    }
+
+    fseek(arq, pos_seek, SEEK_SET);
+    fread(&aluno, sizeof(aluno), 1, arq);
+    if (aluno.matr == matr && aluno.excluido == 0) {
+        *al = aluno;
+        return 1;
+    }
+    return 0;
+}
+
+//quando um aluno é excluido, ele é removido da lista de ativos e adicionados na lista de exluidos;
+void exclui(FILE* arq, int matr, indice** ativos, indice** excluidos) 
+{
+    reg_aluno aluno;
+    int pos_seek = indice_pesquisa(*ativos, matr);
+
+    if (pos_seek == -1)
+    {
+        return 0;
+    }
+
+    fseek(arq, pos_seek, SEEK_SET);
+    fread(&aluno, sizeof(aluno), 1, arq);
+    aluno.excluido = 1;
+
+    printf("Excluindo: %s\n", aluno.nome);
+
+    fseek(arq, pos_seek, SEEK_SET);
+    fwrite(&aluno, sizeof(aluno), 1, arq);
+    indice_insere(excluidos, matr, pos_seek);
+    indice_exclui(ativos, matr);
+}
+
+//procura na lista de excluídos uma posição válida, se não encontrar, insere no final do arquivo
+void inclui(FILE* arq, indice** ativos, indice** excluidos)
+{
+    reg_aluno aluno;
     printf("Informe os dados do aluno (matr, nome, nota1 e nota2) \n");
     scanf("%d%s%d%d", &aluno.matr, aluno.nome, &aluno.nota1, &aluno.nota2);
+
+    int pos_seek = indice_pesquisa(*excluidos, aluno.matr);
+
+    if (pos_seek == -1)
+    {
+        fseek(arq, 0, SEEK_END);
+        pos_seek = ftell(arq);
+    }
+
     aluno.excluido = 0;
+    indice_insere(ativos, aluno.matr, pos_seek);
+    indice_exclui(excluidos, aluno.matr);
     fwrite(&aluno, sizeof(aluno), 1, arq);
 }
 void main() {
     int matr, op;
     reg_aluno aluno;
+    indice* ativos = NULL;
+    indice* excluidos = NULL;
+
     FILE* arq;
-    if (fopen("alunos.dat", "r") == NULL) //access() só pra linux!!!
+
+    if (fopen("alunos.dat", "r") != NULL) //access() só pra linux!!!
         arq = fopen("alunos.dat", "r+"); // arquivo existe
     else
         arq = fopen("alunos.dat", "w+"); // arquivo nao existia
+
+    indice_inicializa(&ativos, &excluidos, arq);
+
     do {
         printf("\nMenu\n 1. Mostrar todos\n 2. Pesquisar\n 3. Incluir\n 4. Excluir\n 5. Sair\nInforme uma opcao: ");
         scanf("%d", &op);
@@ -100,7 +221,7 @@ void main() {
         case 2:
             printf("\nDigite a matricula a ser buscada: ");
             scanf("%d", &matr);
-            if (pesquisa(arq, matr, &aluno)) {
+            if (pesquisa(arq, matr, &aluno, ativos)) {
                 printf("\nAluno encontrado:\n");
                 printf("%d\t%s\t%d\t%d\n", aluno.matr, aluno.nome, aluno.nota1, aluno.nota2);
             }
@@ -108,12 +229,12 @@ void main() {
                 printf("\nA matricula %d nao foi encontrada!\n", matr);
             break;
         case 3:
-            inclui(arq);
+            inclui(arq, &ativos, &excluidos);
             break;
         case 4:
             printf("\nDigite a matricula a ser excluida: ");
             scanf("%d", &matr);
-            exclui(arq, matr);
+            exclui(arq, matr, &ativos, &excluidos);
             break;
         case 5:
             printf("\nSaindo...\n\n");
